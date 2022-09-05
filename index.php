260399d5889a6fbe9123390cff8795f36e4ad647
Yoel Melki <ymelki@gmail.com>
2022-09-05 10:15:16 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+ <?php
+include "header.php";
+ ?>
+    <?php
+    // generer du html en affichant ce qu on a ecrit dans le echo 
+
+    // ALGO
+
+    /*
+    1. VARIABLES
+    2. LES CONDITIONS
+    3. LES BOUCLES
+    4. LES FONCTIONS ...
+    
+    */
+
+    /**
+     * EX AMAZON : IL VA CHERCHE DANS UNE B.D. LES INFO SUR UN PRODUIT DANS LA PAGE PRODUIT
+     * 
+     *1.  VARIABLE : SQL => NOM_PRODUIT =TROTINETTE 
+     * LOCAL (FONCTION)
+     * GLOBAL (PAGE)
+     * SUPER GLOBAL (SESSION (FICHIER COTE SERVEUR) COOKIE (FICHIER COTE CLIENT)   GET (URL) POST (FORMULAIRE CACHE LES MDP) )
+     * 
+     *2.  CONDITION : CREER UNE INTERFACE ADAPTE SELON LES BESOIN : EX: utilisateur qui a prime on lui affiche ses avantages PRIME
+     *
+     * 3. Boucles : Pour travailler sur un ensemble d'information que l'on va deplier
+     * 
+     * 4. Les fonctions : Bout de code qui va s'encapsuler avec un nom
+     * 
+     * 
+     */
+
+     /**
+      * 
+      ***
+      */
+
+     var_dump($GLOBALS);
+
+    
+     ?>
+
+     du html 
+
+     <?php
+
+     ?>
+
+MON FOOTER ....
+</body>
+</html>

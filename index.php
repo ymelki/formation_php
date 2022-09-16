2b9cebd057b1ce0879dc5b30600e83c2702a2b0f
Yoel Melki <ymelki@gmail.com>
2022-09-16 09:59:44 +0200

    first commit

--- index.php
+++ index.php
@@ -36,11 +36,218 @@ include "header.php";
       ***
       */
 
-     var_dump($GLOBALS);
+
+      $produit_nom="XIAOMI M365"; // chaine de caracterev: string
+      $produit_prix=500; // nombre : prix : integer
+      $produit_prix=$produit_prix   +100;
+
+      $produit_prix_tva= $produit_prix * 1.2 ; 
+
+      echo gettype($produit_nom);
+
+      // concatener ?
+      echo "  Nom de la trotinette :     ".$produit_nom." Prix : ".$produit_prix      ; 
+?>
+<hr>
+<?php
+      $produit1=["XIAOMI M365", 500 , 520, "Trotinette derniere generation! "];
+      $user=["Yoel","Melki",35,true,false];
+
+      // afficher un tableau en mode test ! 
+      echo $produit1[0]." ".$produit1[1];
+
+      // changer le prix 500 à 600
+      $produit1[1]=($produit1[1])+100;
+      var_dump($produit1);
+      $age=14;
+
+
+     $mystring = 'abc';
+    $findme   = 'c';
+    $pos = strpos($mystring, $findme);
+
+    echo $pos;
+
+      // si la variable produit nom contient xiaomi on ecrit "c'est la marque xiaomi ! si non autre que marque XIAOMI"
+
+      
+      if (  strtoupper($produit1[0])  == strtoupper("XIAOMI")  ){
+        echo "OKKKKKKK ";
+      }
+
+
+      if (  $age >18   ) {
+        echo "majeur ! ";
+      } 
+      else {
+        echo "mineur ! ";
+      }
+
+      // Boucle
+
+      // Afficher les nombre de 0 à 50
+      // boucle FOR
+      for (   $i=0;   $i<50;   $i=$i+1   ) { 
+
+        echo "je suis yoel"; 
+      }
+      $user=["Yoel","Melki",35,true,false];
+
+
+      for (   $i=0;   $i<count($user);   $i++  ) { 
+
+        echo $user[$i];
+      }
+
+    /*  $i=0;
+
+        while  (  $i<10      ){
+            echo "bonjour";
+             if ( $val==2  ){ $i++; }
+
+        }
+ */
+
+      $arr = array(1, 2, 3, 4);
+      var_dump($arr);
+        foreach ($arr as $key=>$value) {
+          echo $key.'=>'.$value;
+        }
+
+
+      // affiche moi le caractere * 50
+
+      // 1. affiche moi la table de multiplication de 2 
+
+      /**
+       * Boucle Repetition de 10
+       *   Affiche : Calcul 2 * 1   = resultat 1
+       */
+
+      // 2. affiche moi la table de multiplication de 8 
+
+      /**
+       * Table de 1
+       * 1*1=..
+       * 1*2=..
+       * 
+       * 
+       * Table de 2
+       * 
+       * 
+       * 
+       * 
+       * Table de 3
+       * 
+       * Table de 4 
+       * 
+       * ...
+       * 
+       * 
+       * 
+       * Boucle qui va afficher {
+       * Table de $i 
+       *    Boucle de la multipliaction $j
+       * 
+       * }
+       * 
+       * 
+       * 
+       */
+
+
+/**
+ * 
+ * fonctions
+ * 
+ * afficher "hello world"
+ * 
+ * retourner une valeur
+ */
+// declaration de la fonction : CORP
+function mafonction( $information  ){
+  echo "voici l'info : ".$information;
+}
+
+mafonction(  "test"   );
+
+function afficher()  {
+    echo "Hello World !";
+}
+  
+ function test4($prenom){
+  $mavar="Bonjour Mr ".$prenom;
+  return $mavar;
+ }
+ // 1. CREER UNE FONCTION QUI PREND EN PARAMETRE UN Chaine de carractere et qui affiche Bonjour suivi de la chaine de caractere
+ // 2. CREER UNE FONCTION QUI PREND EN PARAMETRE UN Chaine de carractere et qui retourne Bonjour suivi de la chaine de caractere
+ // 3.  CREER UNE FONCTION QUI PREND EN PARAMETRE UN nombre qui affiche le nombre
+ // 3.  CREER UNE FONCTION QUI PREND EN PARAMETRE UN nombre qui retorune le nombre
+  // 3.  CREER UNE FONCTION QUI PREND EN PARAMETRE UN nombre qui affiche si le nombre est > 18 ca ecrit majeur si non mineur
+   // 3.  CREER UNE FONCTION QUI PREND EN PARAMETRE UN nombre qui affiche si le nombre est > 18 ca renvoie true ou false
+ afficher();
+
+
+
+ //compte(300);
 
     
      ?>
+     <?php
+$paramNb=5;
+// créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le resultat de 1+2+3+4+5 
+// Retourner une valeur ?
+// variable local et global
+/*
+$mavar="ce que je veux ! ";
+function mafonction2(){
+  $mavar="ce que je veux ! "; // VARIABLE LOCAL QUI EXISTE QUE DANS LA FONCTION
+  return $mavar;
+}
+echo $mavar;
+*/
 
+
+function resultSomme($paramNb) {
+    for($i=1; $i<6; $i++) {
+        echo ($paramNb+$i) . '<br>';
+    }
+}
+resultSomme(10);
+
+ resultSomme($paramNb);
+echo "-------------------------<br>";
+// créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le resultat de 1x2x3x4x5
+function resultMulti($paramNb) {
+    for($i=1; $i<6; $i++) {
+        echo ($paramNb*$i) . '<br>';
+    }
+}
+resultMulti($paramNb);
+echo "-------------------------<br>";
+// créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le calcul et le resultat de 1+2+3+4+5
+function calcResultSomme($paramNb) {
+    for($i=1; $i<6; $i++) {
+        echo $paramNb . '+' . $i . '=' . ($paramNb+$i) . '<br>';
+    }
+}
+calcResultSomme($paramNb);
+echo '-------------------------<br>';
+
+// créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le calcul et le resultat de 1x2x3x4x5
+function calcResultMulti($paramNb) {
+    for($i=1; $i<6; $i++) {
+        echo $paramNb . 'x' . $i . '=' . ($paramNb*$i) . '<br>';
+    }
+}
+calcResultMulti($paramNb);
+
+    ?>
+     
+     
+     <br />
+----------------------------
+----------------------------
      du html 
 
      <?php
---
+++ index2.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+$tableau= [ 12,34,34  ];
+echo $tableau[2];
+$tableau[2]="toto";
+$tabbleau[]="tRET";
+*/
+
+function resultSomme($paramNb) {
+    $tab=array(); // initialiser un tableau
+    $info=0;   //initialiser une variable stockant l evolution de i
+
+    var_dump($tab);
+    for($i=1; $i<=$paramNb  ; $i++) {
+
+        $info=$i + $info;
+
+
+        $tab[]=$info;
+        var_dump($tab);
+        echo ($paramNb+$i) . '<br>';
+    }
+
+    return $tab[count($tab)-1];
+
+
+}
+
+echo resultSomme(5);
+
+
+// RECUPER UNE INFORMATION : UNE VARIABLE $result reçoit la derniere info
+// return $result
+// return $tab[$paramNb] $tab$tab.count()
+// 1 + 2 +3 +4 +5
+// BOUCLE
+//   vARIBLE RESULTAT STOCKE <= l evolution de i 
+?>
